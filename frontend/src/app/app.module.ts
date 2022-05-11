b7a9d7350986e0ec47e9a9f6e60eb1a231bd3814
Herli Khoury <herli.kh@gmail.com>
2022-05-11 09:32:34 -0300

    pending changes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -16,7 +16,6 @@ import {MatListModule} from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { RedDirective } from './directives/red.directive';
-import { ForDirective } from './directives/for.directive';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 
 @NgModule({
@@ -28,7 +27,6 @@ import { ProductCreateComponent } from './components/product/product-create/prod
     HomeComponent,
     ProductCrudComponent,
     RedDirective,
-    ForDirective,
     ProductCreateComponent
   ],
   imports: [
