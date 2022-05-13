8d9f7952eca681236aaf1b136f01a6ba7ba1ec29
Herli Khoury <herli.kh@gmail.com>
2022-05-12 21:53:54 -0300

    update method

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -37,5 +37,10 @@ export class ProductService {
     return this.http.get<Product>(url)
   }
 
+  update(product: Product): Observable<Product>{
+    const url = `${this.baseUrl}/${product.id}`
+    return this.http.put<Product>(url, product)
+  }
+
 
 }
