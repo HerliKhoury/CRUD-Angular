393f6804ba4a298d93889937483322a0052029f9
Herli Khoury <herli.kh@gmail.com>
2022-05-17 19:46:12 -0300

    header delete-product

--- frontend/src/app/components/product/product-delete/product-delete.component.ts
+++ frontend/src/app/components/product/product-delete/product-delete.component.ts
@@ -2,6 +2,7 @@ import { Product } from './../product.model';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ProductService } from '../product.service'
+import { HeaderService } from '../../template/header/header.service';
 
 @Component({
   selector: 'app-product-delete',
@@ -15,8 +16,14 @@ export class ProductDeleteComponent implements OnInit {
   constructor(
     private productService: ProductService,
     private router: Router,
-    private route: ActivatedRoute
-  ) { }
+    private route: ActivatedRoute,
+    private headerService: HeaderService
+    ) {
+      headerService.headerData = {
+        title:'Delete weapon',
+        icon:'delete_forever',
+        routeUrl:'products/delete/:id'
+      } }
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id')
